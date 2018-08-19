210884c071e47178a05bdc51f128a0c43ade4e33
pc <pc@192.168.2.15>
2018-08-20 00:43:40 +0530

    this is github

---
+++ Test.Jenkins/src/main/java/Test/Jenkins/Test/Jenkins/App.java
@@ -0,0 +1,13 @@
+package Test.Jenkins.Test.Jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Test.Jenkins/src/main/java/Test/Jenkins/Test/Jenkins/Hello.java
@@ -0,0 +1,10 @@
+package Test.Jenkins.Test.Jenkins;
+
+public class Hello {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Hellllooooooo welcome");
+	}
+
+}
